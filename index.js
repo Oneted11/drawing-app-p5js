4e9cd45bc14d650e01ff9bb5ac54a8d30576d554
Oneted11 <muriithited@gmail.com>
2022-08-31 19:53:31 +0300

    feat:working on it working

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // window.onload(console.log("Hello There"));
+// setup p5js
+function setup() {
+  //create full width + height
+  createCanvas(window.innerWidth, window.innerHeight);
+  // add white background
+  background(255);
+}
+
+function draw() {
+  //disable fill
+  noFill();
+
+  if (mouseIsPressed) {
+    //store xy of mouse
+    const point = {
+      x: mouseX,
+      y: MouseY,
+    };
+    //add xy to path i.e. line
+    path.push(point);
+  }
+  //actually draw on canvas
+  strokeWeight(4);
+  stroke(237, 34, 93);
+  beginShape();
+  //for all the points mouse is pressed add vertex
+  path.forEach((point) => {
+    // stroke(51);
+    vertex(point.x, point.y);
+  });
+  endShape;
+}
